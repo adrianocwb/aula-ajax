cd3ee59b0738bf3fb8c10b89500b015e8be4dc24
Adriano <firion_@msn.com>
2019-01-16 21:53:51 -0200

    Form cadastro

---
+++ cadastro.js
@@ -0,0 +1,28 @@
+//executa quando o documento terminar de renderizar (.ready)
+$(document).ready(function(){
+
+    $("#cadastrar").click(function(){
+        var cidade = $("#cidade").val();
+        var cep = $("#cep").val();
+        var conteudo = "<li>" + cidade 
+        + '-CEP:' + cep
+        + ' <span class="del">[X]</span>"</li>';  
+        
+
+        $("#lista-cidades").append(conteudo);
+    }); //fim do click botão
+    
+
+     //fim click del
+
+        $("#lista-cidades").on("click", ".del",function(){
+            $(this).parent().fadeOut(800, function(){
+                $(this).remove();
+            });
+        });
+
+
+}); //fim do ready
+
+
+
